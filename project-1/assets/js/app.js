edce416d4d040271cf53d2a0568e5d8a5e6aa0c6
Matinrodini <149383975+Matinrodini@users.noreply.github.com>
2024-07-22 18:38:55 +0330

    Add files via upload

---
+++ project-1/assets/js/app.js
@@ -0,0 +1,102 @@
+// let slideIndex =  1;
+// showSlides(slideIndex)
+
+// // next/prev ctrl
+// function plusSlide(n) {
+//     showSlides(slideIndex +=n)
+// }
+
+
+// // thumbnail image ctrl
+// function currentSlide(n){
+//     showSlides(slideIndex = n)
+// }
+
+// function showSlides (n) {
+//     let i ; 
+//     let slides = 
+//     document.querySelector(".slide");
+//     let dots = 
+//     document.querySelector(".dots");
+//     if(n > slides.length){
+//         slideIndex = 1
+//     }
+//     if(n < 1){
+//         slideIndex = slides.length
+//     }
+//     for(i = 0 ; i < slides.length; i++){
+//         slides[i].style.display = "none";
+//     }
+//     for(i = 0 ; i < dots.length; i++ ){
+//         dots[i].className = 
+//         dots[i].className.replace("active" ,"")
+//     }
+//     slides[slideIndex-1].style.display = "flex";
+//     dots[slideIndex-1].className += "active";
+// }
+
+
+
+
+
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("slide");
+//   let dots = document.getElementsByClassName("dots");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "flex";
+//   dots[slideIndex-1].className += " active";
+// }
+
+
+let slideIndex= 0;
+showSlides();
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dots");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000);
+   // Change image every 2 seconds
+}
